a5678b425d15bfd4897c776af369f7c229f3cadc
Baptiste LE MORLEC <baptistelemorlec@gmail.com>
2025-02-26 13:15:25 +0100

    Health check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use axum::{http::StatusCode, response::IntoResponse, routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let port = env::var("PORT").unwrap_or("8080".to_owned());
+
+    let app = Router::new().route("/health", get(health_check));
+    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{port}"))
+        .await
+        .unwrap();
+
+    println!("Starting server on port {port}");
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn health_check() -> impl IntoResponse {
+    println!("health check");
+    (StatusCode::OK, "OK")
 }
